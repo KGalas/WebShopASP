e0c73b0ab91dcaaa257c2e3d09d8051182631d31
Krzysztof Galas <kagzbb@gmail.com>
2020-11-26 20:32:08 +0100

    Dodanie atrybutów

--- WebShop/WebShop/Models/Kategoria.cs
+++ WebShop/WebShop/Models/Kategoria.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebShop.Models
 {
     public class Kategoria
     {
         public int KategoriaID { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategorii { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź opis kategorii")]
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
         public virtual ICollection<Kurs> Kurs { get; set; }
--- WebShop/WebShop/Models/Kurs.cs
+++ WebShop/WebShop/Models/Kurs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,12 +13,17 @@ namespace WebShop.Models
 
         public int KategoriaID { get; set; }
 
+        [Required(ErrorMessage ="Wprowadź nazwę kursu")]
+        [StringLength(100)]
         public string TytulKursu { get; set; }
 
+        [Required(ErrorMessage = "Wprowadź nazwę autora")]
+        [StringLength(100)]
         public string AutorKursu { get; set; }
 
         public DateTime DataDodania { get; set; }
 
+        [StringLength(100)]
         public string NazwaPlikuObrazka { get; set; }
 
         public string OpisKursu { get; set; }
